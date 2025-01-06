e2ad0bcaaad5168f072a7bafe8fab40425162627
Daniel Nozka <danozka@gmail.com>
2025-01-06 14:41:17 +0100

    Reduces logging verbosity

--- src/trading_bot/__main__.py
+++ src/trading_bot/__main__.py
@@ -81,10 +81,6 @@ def main(
 
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        format='%(asctime)s.%(msecs)03d - [%(levelname)s] - [%(name)s] - %(message)s',
-        datefmt='%d-%m-%Y %H:%M:%S',
-        level=logging.INFO
-    )
+    logging.basicConfig(format='%(asctime)s.%(msecs)03d - %(message)s', datefmt='%d-%m-%Y %H:%M:%S', level=logging.INFO)
     container: Container = Container()
     typer.run(main)
--- src/validation/__main__.py
+++ src/validation/__main__.py
@@ -27,7 +27,7 @@ def main(
 
 if __name__ == '__main__':
     logging.basicConfig(
-        format='%(asctime)s.%(msecs)03d - [%(levelname)s] - [%(name)s] - %(message)s',
+        format='%(asctime)s.%(msecs)03d - %(message)s',
         datefmt='%d-%m-%Y %H:%M:%S',
         level=logging.DEBUG
     )
