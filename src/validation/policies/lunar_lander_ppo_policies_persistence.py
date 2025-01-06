3e8249b20cf667c200b222f35d9539e0fff86cd8
Daniel Nozka <danozka@gmail.com>
2025-01-06 14:44:03 +0100

    Removes unused code

--- src/validation/policies/lunar_lander_ppo_policies_persistence.py
+++ src/validation/policies/lunar_lander_ppo_policies_persistence.py
@@ -1,6 +1,5 @@
 import logging
 from logging import Logger
-from pathlib import Path
 from uuid import UUID
 
 from reinforcement_learning import IPpoPoliciesPersistence
@@ -9,10 +8,6 @@ from validation.policies.lunar_lander_ppo_policy import LunarLanderPpoPolicy
 
 class LunarLanderPpoPoliciesPersistence(IPpoPoliciesPersistence):
     _log: Logger = logging.getLogger(__name__)
-
-    def __init__(self, ppo_policies_directory: Path = Path('./ppo-policies')) -> None:
-        self._ppo_policies_directory = ppo_policies_directory
-        self._ppo_policies_directory.mkdir(parents=True, exist_ok=True)
 
     def load_ppo_policy(self, ppo_policy_id: UUID) -> LunarLanderPpoPolicy:
         self._log.debug('Creating Lunar Lander PPO policy...')
